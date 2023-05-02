f1845b14820092788038ddc05f97e24f789e4844
Dida En <firdanb@students.unnes.ac.id>
2023-05-03 03:48:21 +0700

    Membuat function gridTemplate() pada script.js sebagai awalah function.

---
+++ script.js
@@ -0,0 +1,9 @@
+// input : persamaan latex
+// output : ukuran grid-template-columns dan grid-template-rows
+
+// Gimana caranya bikin masukan persamaan latex tuh??
+// Apa gak persamaannya ditampilin dulu baru dihitung grid template-nya??
+
+function gridTemplate() {
+
+}
